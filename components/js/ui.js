14dbf20454924f6cb17ddb1214e3346ff53e5b77
Stefan <rosu.stefan96@gmail.com>
2017-08-03 18:04:57 +0300

    confirm page input values

--- components/js/ui.js
+++ components/js/ui.js
@@ -1,14 +1,1 @@
 $('#date').datepicker();
-
-
-// $('#date').on('changeDate',function(){
-// 	$('#hidden_input').val($('#date').datepicker('getFormattedDate'));
-// });
-
-
-// $(document).on("click","#iubi",function(){
-// 	var fname = $("#hidden_input").val();
-
-// 	alert(fname);
-
-// });
--- custom.js
+++ custom.js
@@ -15,10 +15,21 @@
 		$(".details-container").show();
 	}
 	function timeBack() {
+		var d = document.getElementById("date").value;
+		document.getElementById("time").innerHTML = "Date:  " + d;
 		$(".time-container").show();
 		$(".details-container").hide();
 	}
 	function confirmNext() {
+		 var f = document.getElementById("f-name").value;
+         var l = document.getElementById("l-name").value;
+         var e = document.getElementById("Email").value;
+         var m = document.getElementById("Mobile").value;
+         document.getElementById("first-name").innerHTML = "First Name:  " + f;
+         document.getElementById("last-name").innerHTML = "Last Name:  " + l;
+         document.getElementById("email").innerHTML = "Email:  " + e;
+         document.getElementById("mobile").innerHTML = "Mobile:  " + m;
+
 		$(".confirm-container").show();
 		$(".details-container").hide();
 	}
